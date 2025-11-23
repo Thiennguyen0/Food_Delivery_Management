26b3ed8c5817617fddf467a5dfe634d0e19017b8
Thiennguyen0 <huuthien140209@gmail.com>
2025-11-23 19:50:51 +0700

    added tabs

--- testingui.py
+++ testingui.py
@@ -1,68 +1,142 @@
 import customtkinter
 
-# def hide_all_widgets(parent_widget):
-#     for widget in parent_widget.winfo_children():
-#         widget.pack_forget()
-
-
-
-customtkinter.set_appearance_mode("system")
+customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("dark-blue")
+
+#global functions
+
+def show_frame(root, sframe):
+    for button in root.SelectionBar.winfo_children()[1:]:
+        if button.cget("text") == sframe.__name__:
+            eval(f"root.SelectionBar.{sframe.__name__}.configure(fg_color='#1f538d')")
+        else:
+            eval(f"root.SelectionBar.{button.cget("text")}.configure(fg_color=['gray90', 'gray15'])")
+
+    for frame in root.winfo_children()[1:]:
+        if not isinstance(frame, sframe):
+            frame.grid_forget()
+        else:
+            frame.grid(row=0, column=1, padx=20, pady=20, sticky="nsew")
+
+
+
+#Class definition
 
 class Selection_Frame(customtkinter.CTkFrame):
     def __init__(self, master):
         super().__init__(master)
 
-        self.Label = customtkinter.CTkLabel(self, text="Sidebar", font=("Arial", 55, "bold"), pady=25)
-        self.Label.pack(pady=30)
+        self.Label = customtkinter.CTkLabel(self, text="Food Delivery\nManagement", font=("Arial", 55, "bold"), pady=25)
+        self.Label.grid(row = 0, column = 0, padx = 20, pady=20, sticky="ew")
+
+        self.Dashboard = customtkinter.CTkButton(self, text="Dashboard", font=("Arial", 24), command=self.Afunc, height=70, corner_radius=20, fg_color=["gray90", "gray15"], border_color=["gray90", "gray12"], border_width=2)
+        self.Dashboard.grid(row = 1, column = 0, padx = 10, pady=20, sticky="ew")
+
+        self.Customers = customtkinter.CTkButton(self, text="Customers", font=("Arial", 24), command=self.Bfunc, height=70, corner_radius=20, fg_color=["gray90", "gray15"], border_color=["gray90", "gray12"], border_width=2)
+        self.Customers.grid(row = 2, column = 0, padx = 10, pady=20, sticky="ew")
+
+        self.Dishes = customtkinter.CTkButton(self, text="Dishes", font=("Arial", 24), command=self.Cfunc, height=70, corner_radius=20, fg_color=["gray90", "gray15"], border_color=["gray90", "gray12"], border_width=2)
+        self.Dishes.grid(row = 3, column = 0, padx = 10, pady=20, sticky="ew")
+
+        self.Ingredients = customtkinter.CTkButton(self, text="Ingredients", font=("Arial", 24), command=self.Dfunc, height=70, corner_radius=20, fg_color=["gray90", "gray15"], border_color=["gray90", "gray12"], border_width=2)
+        self.Ingredients.grid(row = 4, column = 0, padx = 10, pady=20, sticky="ew")
+
+        self.Orders = customtkinter.CTkButton(self, text="Orders", font=("Arial", 24), command=self.Efunc, height=70, corner_radius=20, fg_color=["gray90", "gray15"], border_color=["gray90", "gray12"], border_width=2)
+        self.Orders.grid(row = 5, column = 0, padx = 10, pady=20, sticky="ew")
+
+        self.Employees = customtkinter.CTkButton(self, text="Employees", font=("Arial", 24), command=self.Ffunc, height=70, corner_radius=20, fg_color=["gray90", "gray15"], border_color=["gray90", "gray12"], border_width=2)
+        self.Employees.grid(row = 6, column = 0, padx = 10, pady=20, sticky="ew")
 
 
-        self.buttonA = customtkinter.CTkButton(self, text="buttonA", font=("Arial", 24), height=50, width = 250)
-        self.buttonA.pack(pady=30)
+    def Afunc(self): show_frame(self.master, Dashboard)
+    def Bfunc(self): show_frame(self.master, Customers)
+    def Cfunc(self): show_frame(self.master, Dishes)
+    def Dfunc(self): show_frame(self.master, Ingredients)
+    def Efunc(self): show_frame(self.master, Orders)
+    def Ffunc(self): show_frame(self.master, Employees)
 
-        self.buttonB = customtkinter.CTkButton(self, text="buttonB", font=("Arial", 24), height=50, width = 250)
-        self.buttonB.pack(pady=30)
+# --- 
 
-        self.buttonC = customtkinter.CTkButton(self, text="buttonC", font=("Arial", 24), height=50, width = 250)
-        self.buttonC.pack(pady=30)
+class Dashboard(customtkinter.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
 
-        self.buttonD = customtkinter.CTkButton(self, text="buttonD", font=("Arial", 24), height=50, width = 250)
-        self.buttonD.pack(pady=30)
+        self.Label = customtkinter.CTkLabel(self, text="Dashboard")
+        self.Label.pack()
+        
+class Customers(customtkinter.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
 
-        self.buttonE = customtkinter.CTkButton(self, text="buttonE", font=("Arial", 24), height=50, width = 250)
-        self.buttonE.pack(pady=30)
+        self.Label = customtkinter.CTkLabel(self, text="Customers")
+        self.Label.pack()
 
-        self.buttonF = customtkinter.CTkButton(self, text="buttonF", font=("Arial", 24), height=50, width = 250)
-        self.buttonF.pack(pady=30)
-    
-    def Afunc(self):
-        pass
+class Dishes(customtkinter.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
+        
+        self.Label = customtkinter.CTkLabel(self, text="Dishes")
+        self.Label.pack()
 
-    def Bfunc(self):
-        pass
-    
-    def Cfunc(self):
-        pass
-    
-    def Dfunc(self):
-        pass
-    
-    def Efunc(self):
-        pass
-    
-    def Ffunc(self):
-        pass
+class Ingredients(customtkinter.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
+        
+        self.Label = customtkinter.CTkLabel(self, text="Ingredients")
+        self.Label.pack()
+
+class Orders(customtkinter.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
+
+        self.Label = customtkinter.CTkLabel(self, text="Orders")
+        self.Label.pack()
+
+class Employees(customtkinter.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
+
+        self.Label = customtkinter.CTkLabel(self, text="Employees")
+        self.Label.pack()
+
 
 
 class Root(customtkinter.CTk):
-    def __init__(self):
+    def __init__(self, selected_frame):
         super().__init__()
 
         self.title("Food Delivery Management")
         self.geometry("1280x720")
 
-        self.SelectionBar = Selection_Frame(master=self)
-        self.SelectionBar.grid(row=0, column=0, padx=20, pady=20)
+        self.rowconfigure(0, weight=1)
+        self.columnconfigure(1, weight=2)
 
-root = Root()
+        self.SelectionBar = Selection_Frame(master=self)        
+        self.SelectionBar.grid(row=0, column=0, padx=(20, 0), pady=20, sticky="nsew")
+
+        self.Dashboard = Dashboard(master=self)
+        self.Dashboard.grid(row=0, column=1, padx=20, pady=20, sticky="nsew")
+
+        self.Customers = Customers(master=self)
+        self.Customers.grid(row=0, column=1, padx=20, pady=20, sticky="nsew")
+
+        self.Dishes = Dishes(master=self)
+        self.Dishes.grid(row=0, column=1, padx=20, pady=20, sticky="nsew")
+
+        self.Ingredients = Ingredients(master=self)
+        self.Ingredients.grid(row=0, column=1, padx=20, pady=20, sticky="nsew")
+
+        self.Orders = Orders(master=self)
+        self.Orders.grid(row=0, column=1, padx=20, pady=20, sticky="nsew")
+
+        self.Employees = Employees(master=self)
+        self.Employees.grid(row=0, column=1, padx=20, pady=20, sticky="nsew")
+
+        show_frame(self, selected_frame)
+
+
+   
+
+#Mainloop
+root = Root(Dashboard)
 root.mainloop()
